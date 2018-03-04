3763f60f264682f758bd4b361634d37163be7b25
Andy Bryant <a.mcdermid.bryant@gmail.com>
2018-03-04 11:29:41 -0800

    fixed test

--- test/test-server.js
+++ test/test-server.js
@@ -1,15 +1,13 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const {app} = require('../index');
-
 const should = chai.should();
 chai.use(chaiHttp);
 
 describe('API', function() {
 
     it('should 200 on GET requests', function() {
-        return chai.request(app)
+        return chai.request('https://powerful-caverns-35930.herokuapp.com')
             .get('/beerNode/cQMfwv')
             .then(function(res) {
                 res.should.have.status(200);
